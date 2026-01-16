3c4db16c9b2de538645c4dd0641e40833ce8725b
Daniel Martins <danieldefmartins@gmail.com>
2026-01-16 17:36:45 -0500

    run

--- screens/UniversalAddScreen.tsx
+++ screens/UniversalAddScreen.tsx
@@ -33,7 +33,7 @@ import {
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import { supabase } from '../lib/supabase';
+import { supabase } from '../lib/supabaseClient';
 
 // Import category components
 import { CategorySelector } from '../components/CategorySelector';
